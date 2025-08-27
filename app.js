d2f71c3dccbb03043aafb593130a85f69102ebe7
Larissa Valli <larissavalli4@gmail.com>
2025-08-27 11:30:45 -0300

    desafio concluido

--- app.js
+++ app.js
@@ -22,3 +22,59 @@ function verificarChute() {
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1);
 }
+
+
+//DESAFIO 02
+
+// Criar uma função que exibe "Olá, mundo!" no console.
+
+function exibirOla() {
+    console.log('Olá mundo!');
+}
+
+exibirOla();
+
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+function exibirNome(nome) {
+    console.log(`Olá, ${nome}`);
+}
+
+exibirNome("Alice");
+
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+function exibirNumero(numero) {
+    return numero * 2;
+}
+
+let resultadoDobro = calcularDobro(5);
+console.log(resultadoDobro);
+
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function tresNumeros(a, b, c) {
+    return (a, b, c) / 3;
+}
+
+let mediaNumero = mediaDosNumeros(4, 7, 10);
+console.log(mediaNumero);
+
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+function doisNumeros(a, b) {
+    return a > b ? a : b;
+}
+
+let maiorNumero = encontrarMaior(15, 9);
+console.log(maiorNumero);
+
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function quadrado(numero) {
+  return numero * numero;
+}
+
+let resultado = quadrado(2);
+console.log(resultado); 
